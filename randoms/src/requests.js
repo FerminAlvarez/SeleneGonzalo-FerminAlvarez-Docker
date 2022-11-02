ebe9c0de3442ce1d32078df08bc17ff9cba8683b
Fermin Alvarez <feeralvarez013@gmail.com>
2022-11-02 17:39:40 -0300

    Imagen de php en dos instancias en dos puertos

--- randoms/src/requests.js
+++ randoms/src/requests.js
@@ -1,8 +1,9 @@
 // El objetivo es reemplazar esto por microservicios que oculten la fuente de datos.
 const requests = [
-  {"url":"http://localhost:3004","color":"pink"},
-  {"url":"http://localhost:3005","color":"cyan"},
+  {"url":"http://localhost:3004","color":"blue"},
+  {"url":"http://localhost:3005","color":"red"},
+  {"url":"http://localhost:3006","color":"white"},
   {"url":"http://localhost:5000","color":"yellow"},
 ]
-
+console.log(process.env.EXPRESS_PORT);
 export default requests;
